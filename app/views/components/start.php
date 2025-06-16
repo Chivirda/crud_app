134742c6f2023ca55d48b8b9b165bec88b51cca7
Narfu <safu@narfu.ru>
2025-06-16 19:07:19 +0300

    feat: добавлен новый хедер и logout

--- app/views/components/start.php
+++ app/views/components/start.php
@@ -12,6 +12,7 @@
     <title>Планировщик задач</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
